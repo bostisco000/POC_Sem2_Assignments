2e59e20dec65035655daee6bee592f105569078b
Scott Bostic <bostisc000@warren.k12.in.us>
2024-02-08 12:06:45 -0500

    assignment

--- 06_Types, Modules, and Packages/03_Platform and Random Modules/assignment.py
+++ 06_Types, Modules, and Packages/03_Platform and Random Modules/assignment.py
@@ -1,9 +1,7 @@
-#Create a random number generator that simulates a dice roll
-
 import random
 
 dice_rolls = [1, 2, 3, 4, 5, 6]
 
-random_roll = #YOUDO this
+random_roll = random.sample(dice_rolls, 1)
 
 print("You rolled a", random_roll)
