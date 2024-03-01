429ee8ace34f2b703b3994956eb2fb93b59af5c0
Scott Bostic <bostisc000@warren.k12.in.us>
2024-03-01 12:29:06 -0500

    assignment

--- 08_OOP/01_Intro/assignment.py
+++ 08_OOP/01_Intro/assignment.py
@@ -1,15 +1,11 @@
 class RightTriangle:
-    def __init__(self, base, height):
-        self.base = base
-        #YOUDO:  do the same for height
+    def __init__(self, base, height):self.base = base
+
     
-    def area(self):
-        #Youdo return the area which is 1/2 * base * height
-        pass #Remove this pass when finished
+def area(self): 
+    return (1/2) * self.base * self.height
 
-triangle1 = RightTriangle(3, 4)
-print("The area of triangle1 is", triangle1.area())
-
-#YOUDO: make another rightTriangle called triangle2
-#YOUDO:  and print its area.  
-
+triangle_1 = RightTriangle(3, 4)
+print("The area of triange_1 is", triangle_1.area())#Do the same thing for another triangle.
+triangle_2 = RightTriangle(6, 8)
+print("The area of triangle_2 is", triangle_2.area())
