d4afc61a47c5bd88506fde21702a209d6907b1ed
Scott Bostic <bostisc000@warren.k12.in.us>
2024-03-21 12:03:13 -0400

    a

--- 08_OOP/05_Methods/assignment.py
+++
@@ -1,4 +0,0 @@
-
-
-# YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-# using only the methods not the fields/property/attributes
--- 10_Functional Programming/01_Lambda Expressions/assignment.py
+++ 10_Functional Programming/01_Lambda Expressions/assignment.py
@@ -1,4 +1,6 @@
 product = lambda a, b: a * b
+quotient = lambda a, b: a/b
+exponent = lambda a, b: a**b
 #YOUDO for quotient
 #YOUDO for exponent
 
